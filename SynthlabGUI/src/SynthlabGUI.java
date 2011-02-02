c038ccb6ff27549ad5b1492bfe9b49058135529b
mehdikool83 <mehdikool83@71548d6f-1a6a-e512-57bc-3977d4f6448c>
2011-02-02 10:59:34 +0000

    Ajout d'un smiley

--- SynthlabGUI/src/SynthlabGUI.java
+++ SynthlabGUI/src/SynthlabGUI.java
@@ -2,6 +2,6 @@ public class SynthlabGUI
 {
   public static void main(String[] args)
   {
-    System.out.println("Welcome to Synthlab GUI");
+    System.out.println("Welcome to Synthlab GUI :)");
   }
 }
