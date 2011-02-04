670ead23a29e819ca3763064cbcaa7e826b50922
vallee.aurelien <vallee.aurelien@71548d6f-1a6a-e512-57bc-3977d4f6448c>
2011-02-04 09:00:28 +0000

    Add more coherent VCO outputing a sine wave btw -1 and 1

--- Synthlab/src/synthlab/internal/modules/ModuleVCO.java
+++ Synthlab/src/synthlab/internal/modules/ModuleVCO.java
@@ -12,6 +12,10 @@ public class ModuleVCO extends BasicModule
     addInput(new BasicPort("iFrequency", 0));
     addInput(new BasicPort("iConstant", 1));
     addOutput(new BasicPort("oFrequency", 0));
+
+    frameCount_ = 0;
+    frameRate_ = 44100;
+    initialFrequency_ = 440.;
   }
 
   @Override
@@ -19,7 +23,15 @@ public class ModuleVCO extends BasicModule
   {
     double ifreq = getInput("iFrequency").getValue();
     double iconst = getInput("iConstant").getValue();
-    double out = (int) Math.pow(2., (ifreq + iconst));
+    double out = Math.sin((double) frameCount_ / (double) frameRate_ * initialFrequency_ * 2. * Math.PI);
+    out *= (Short.MAX_VALUE);
+    out *= Math.pow(2, ifreq+iconst);
     getOutput("oFrequency").setValue(out);
+
+    frameCount_ = ++frameCount_ % 441400;
   }
+
+  private int    frameCount_;
+  private int    frameRate_;
+  private double initialFrequency_;
 }
