1338086ce45d6e3861d2646e3018000d31728166
matian85 <matian85@71548d6f-1a6a-e512-57bc-3977d4f6448c>
2011-02-08 15:37:57 +0000

    modification d'instanciation de NumberKnobPanel

--- SynthlabGUI/src/widgets/ModuleConfigWindow.java
+++ SynthlabGUI/src/widgets/ModuleConfigWindow.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.Point;
+import java.util.HashMap;
 
 import javax.swing.JDialog;
 import javax.swing.JFrame;
@@ -12,7 +13,8 @@ import javax.swing.JPanel;
 
 import synthlab.api.Module;
 import synthlab.api.Port;
-import widgets.knob.AbstractKnobPanel;
+
+import widgets.configPanel.AbstractConfigPanel;
 import widgets.knob.FunctionKnobPanel;
 import widgets.knob.NumberKnobPanel;
 
@@ -26,28 +28,44 @@ public class ModuleConfigWindow extends JDialog {
    */
   private static final long serialVersionUID = 1L;
   
+  public static HashMap<Port.ValueUnit, String> unitList = new HashMap<Port.ValueUnit, String>();
+  
   /**
    * Le module abstrait
    * */
   private Module module_;
 
+  private void initUnitList() {
+      unitList.put(Port.ValueUnit.AMPLITUDE, "");
+      unitList.put(Port.ValueUnit.PERCENTAGE, "%");
+      unitList.put(Port.ValueUnit.DECIBELS, "db");
+      unitList.put(Port.ValueUnit.HERTZ, "Hz");
+      unitList.put(Port.ValueUnit.MILLISECONDS, "ms");
+      unitList.put(Port.ValueUnit.VOLT, "v");
+      
+  }
   
   public ModuleConfigWindow(synthlab.api.Module module, JFrame parent, Point point) {
       super(parent, true);
-      setLayout(new FlowLayout(FlowLayout.CENTER, 5,5));
-      
+      setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+      initUnitList();
       module_ = module;
       
-      AbstractKnobPanel knob;
+      AbstractConfigPanel knob;
       for(Port p : module_.getInputs()) {
-          if(p.getName().equals("iShape")) //TODO just pour test.  
+          if(p.getValueType() == Port.ValueType.DISCRETE ) { //TODO just pour test.  
               knob = new FunctionKnobPanel(p.getName());
-          else 
-              knob = new NumberKnobPanel(p.getName(), -1.0, 1.0, "", "0.0", !p.isLinked());
+          } else {
+              double min = p.getValueRange().minimum;
+              double max = p.getValueRange().maximum;
+              
+              
+              knob = new NumberKnobPanel(p.getName(), min, max, unitList.get(p.getValueUnit()), "0.0", !p.isLinked());
+          }
           knob.setPort(p);
           add((JPanel)knob);
       }
-           
+      
       if(getContentPane().getComponentCount() == 0) {
 	  add(new JLabel("No configuration for this module."));
       }
