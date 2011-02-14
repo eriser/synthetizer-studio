4d219d2dbb55835c03de525296bd891492f3853e
matian85 <matian85@71548d6f-1a6a-e512-57bc-3977d4f6448c>
2011-02-14 14:05:54 +0000

    debug GUI de reglage

--- SynthlabGUI/src/synthlabgui/widgets/configPanel/knob/numberKnob/NumberKnobPanel.java
+++ SynthlabGUI/src/synthlabgui/widgets/configPanel/knob/numberKnob/NumberKnobPanel.java
@@ -8,155 +8,151 @@ import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.text.DecimalFormat;
 import java.util.Observable;
+
 import javax.swing.JPanel;
+
 import synthlab.api.Port;
 import synthlabgui.widgets.configPanel.AbstractConfigPanel;
 import synthlabgui.widgets.configPanel.knob.AbstractKnob;
 import synthlabgui.widgets.configPanel.knob.KnobEvent;
 import synthlabgui.widgets.configPanel.knob.KnobListener;
 
-public class NumberKnobPanel extends JPanel implements KnobListener, AbstractConfigPanel
-{
-  /**
-   * @param title
-   *          le titre de panneau
-   * 
-   * @param min
-   *          valeur minimale
-   * @param max
-   *          valeur maximale
-   * @param unit
-   *          l'unité de mesure à afficher
-   * @param pattern
-   *          pattern des chiffres
-   * 
-   * @param enable
-   *          vrai si ce panneau est active et dèsative sinon
-   * @param continuous
-   *          vrai pour générer les valeurs continus et discrètes sison
-   * */
-  public NumberKnobPanel(String title, double min, double max, String unit, String pattern, boolean enable,
-      boolean continuous)
-  {
-    this.title = title;
-    maxValue = max;
-    minValue = min;
-    this.unit = unit;
-    this.pattern = pattern;
-    enabled = enable;
-    this.continous = continuous;
-    setLayout(null);
-    setMinimumSize(size);
-    setPreferredSize(size);
-    setSize(size);
-    knob = new NumberKnob();
-    knob.setActive(enable);
-    knob.addKnobListener(this);
-    add(knob);
-    knob.setLocation((size.width - AbstractKnob.size.width) / 2, TITLE_HEIGHT + 5);
-    value = computeValue(knob.getValue());
-  }
-
-  public void paintComponent(Graphics gc)
-  {
-    super.paintComponent(gc);
-    RenderingHints renderHints = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-    renderHints.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-    ((Graphics2D) gc).setRenderingHints(renderHints);
-    // dessine le titre
-    gc.setColor(Color.darkGray);
-    // 100 - (g.getFontMetrics().stringWidth(module_.getName()) / 2;
-    gc.setFont(new Font("Arial", Font.PLAIN, 11));
-    gc.drawString(title, (int) ((getWidth() - gc.getFontMetrics().stringWidth(title)) / 2.) - 2, 10);
-    // dessine la valeur
-    DecimalFormat df = new DecimalFormat(pattern);
-    String str = df.format(value) + " " + unit;
-    gc.setColor(Color.gray);
-    gc.setFont(new Font("Arial", Font.PLAIN, 10));
-    gc.drawString(str, (getWidth() - gc.getFontMetrics().stringWidth(str)) / 2, 75);
-  }
-
-  @Override
-  public void knobTurned(KnobEvent e)
-  {
-    // 0 .. 9999
-    int scale = e.getValue();
-    value = computeValue(scale);
-    notifyPort(value);
-    repaint(0, 0, getWidth(), 20);
-  }
-
-  private double computeValue(int rawValue)
-  {
-    if (continous)
-    {
-      double piece = (maxValue - minValue) / 10000;
-      return minValue + piece * rawValue;
+public class NumberKnobPanel extends JPanel implements KnobListener,
+	AbstractConfigPanel {
+    /**
+     * @param title
+     *            le titre de panneau
+     * 
+     * @param min
+     *            valeur minimale
+     * @param max
+     *            valeur maximale
+     * @param unit
+     *            l'unité de mesure à afficher
+     * @param pattern
+     *            pattern des chiffres
+     * 
+     * @param enable
+     *            vrai si ce panneau est active et dèsative sinon
+     * @param continuous
+     *            vrai pour générer les valeurs continus et discrètes sison
+     * */
+    public NumberKnobPanel(String title, double min, double max, String unit,
+	    String pattern, boolean enable, boolean continuous) {
+	this.title = title;
+	maxValue = max;
+	minValue = min;
+	this.unit = unit;
+	this.pattern = pattern;
+	enabled = enable;
+	this.continous = continuous;
+	setLayout(null);
+	setMinimumSize(size);
+	setPreferredSize(size);
+	setSize(size);
+	knob = new NumberKnob();
+	knob.setActive(enable);
+	knob.addKnobListener(this);
+	add(knob);
+	knob.setLocation((size.width - AbstractKnob.size.width) / 2,
+		TITLE_HEIGHT + 5);
+	value = computeValue(knob.getValue());
     }
-    else
-    {
-      int piece = (int) (10000.0 / (maxValue - minValue));
-      return rawValue / piece;
+
+    public void paint(Graphics gc) {
+	super.paint(gc);
+	RenderingHints renderHints = new RenderingHints(
+		RenderingHints.KEY_ANTIALIASING,
+		RenderingHints.VALUE_ANTIALIAS_ON);
+	renderHints.put(RenderingHints.KEY_RENDERING,
+		RenderingHints.VALUE_RENDER_QUALITY);
+	((Graphics2D) gc).setRenderingHints(renderHints);
+	// dessine le titre
+	gc.setColor(Color.darkGray);
+	// 100 - (g.getFontMetrics().stringWidth(module_.getName()) / 2;
+	gc.setFont(new Font("Arial", Font.PLAIN, 11));
+	gc.drawString(title, (int) ((getWidth() - gc.getFontMetrics()
+		.stringWidth(title)) / 2.) - 2, 10);
+	// dessine la valeur
+	DecimalFormat df = new DecimalFormat(pattern);
+	String str = df.format(value) + " " + unit;
+	gc.setColor(Color.gray);
+	gc.setFont(new Font("Arial", Font.PLAIN, 10));
+	gc.drawString(str,
+		(getWidth() - gc.getFontMetrics().stringWidth(str)) / 2, 75);
+
     }
-  }
 
-  public void notifyPort(double value)
-  {
-    if (inputPort != null && !inputPort.isLinked())
-    {
-      inputPort.setValues(value);
-      // System.out.println("Send " + value);
-      // DecimalFormat df = new DecimalFormat("0.00");
+    @Override
+    public void knobTurned(KnobEvent e) {
+	// 0 .. 9999
+	int scale = e.getValue();
+	value = computeValue(scale);
+	notifyPort(value);
+	repaint(0, 0, getWidth(), 20);
     }
-  }
 
-  public void setPort(Port port)
-  {
-    inputPort = port;
-    if (port != null)
-    {
-      port.addObserver(this);
-      update(port, null);
+    private double computeValue(int rawValue) {
+	if (continous) {
+	    double piece = (maxValue - minValue) / 10000;
+	    return minValue + piece * rawValue;
+	} else {
+	    int piece = (int) (10000.0 / (maxValue - minValue));
+	    return rawValue / piece;
+	}
     }
-  }
 
-  public void setState(boolean enabled)
-  {
-    this.enabled = enabled;
-  }
+    public void notifyPort(double value) {
+	if (inputPort != null && !inputPort.isLinked()) {
+	    inputPort.setValues(value);
+	    // System.out.println("Send " + value);
+	    // DecimalFormat df = new DecimalFormat("0.00");
+	}
+    }
 
-  private static final long serialVersionUID  = 1L;
+    public void setPort(Port port) {
+	inputPort = port;
+	if (port != null) {
+	    port.addObserver(this);
+	    update(port, null);
+	}
+    }
 
-  private double            value;
+    public void setState(boolean enabled) {
+	this.enabled = enabled;
+    }
 
-  private double            maxValue;
+    private static final long serialVersionUID = 1L;
 
-  private double            minValue;
+    private double value;
 
-  private String            unit;
+    private double maxValue;
 
-  private String            pattern;
+    private double minValue;
 
-  private NumberKnob        knob;
+    private String unit;
 
-  private int               numberDisplaySize = 20;
+    private String pattern;
 
-  public static int         TITLE_HEIGHT      = 10;
+    private NumberKnob knob;
 
-  private Dimension         size              = new Dimension(AbstractKnob.size.width + 35 + 2, 20 + 2
-                                                  + AbstractKnob.size.height + TITLE_HEIGHT + numberDisplaySize);
+    private int numberDisplaySize = 20;
 
-  private String            title;
+    public static int TITLE_HEIGHT = 10;
 
-  private boolean           enabled           = true;
+    private Dimension size = new Dimension(AbstractKnob.size.width + 35 + 2, 20
+	    + 2 + AbstractKnob.size.height + TITLE_HEIGHT + numberDisplaySize);
 
-  private boolean           continous         = true;
+    private String title;
 
-  private Port              inputPort;
+    private boolean enabled = true;
 
-  @Override
-  public void update(Observable o, Object arg)
-  {
-    value = ((Port) o).getValues().getDouble(0);
-  }
+    private boolean continous = true;
+
+    private Port inputPort;
+
+    @Override
+    public void update(Observable o, Object arg) {
+	value = ((Port) o).getValues().getDouble(0);
+    }
 }
