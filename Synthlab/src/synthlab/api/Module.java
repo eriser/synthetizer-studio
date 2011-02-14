9202fb8dcb759288a33fa074ea2fd29f7aff1a6a
vallee.aurelien <vallee.aurelien@71548d6f-1a6a-e512-57bc-3977d4f6448c>
2011-02-14 17:20:57 +0000

    Documentation

--- Synthlab/src/synthlab/api/Module.java
+++ Synthlab/src/synthlab/api/Module.java
@@ -2,41 +2,97 @@ package synthlab.api;
 
 import java.util.List;
 
-
+/**
+ * This is the module interface. It represents an analog synthesizer module having
+ * input and output ports.
+ */
 public interface Module
 {
-  // --- Name
+  //================================================ Name
+  /**
+   * This is the name of the module ( e.g. "VCO", "VCF" ... )
+   */
   public String getName();
 
-  // --- Inputs
+  //================================================ Inputs
+  /**
+   * Add an input port to the module. This port will not be copied, but
+   * rather shared (i.e. you can modify it, but be sure to synchronize
+   * it beforehand)
+   */
   public void addInput(Port input);
 
+  /**
+   * Add input ports to the module. These ports will not be copied, but
+   * rather shared (i.e. you can modify them, but be sure to synchronize
+   * them beforehand)
+   */
   public void addInputs(List<Port> inputs);
 
+  /**
+   * The port with the specified name will be removed from the list of
+   * input ports.
+   * @param name the name of the port to be removed
+   */
   public void removeInput(String name);
 
+  /**
+   * The ports with the specified names will be removed from the list of
+   * input ports.
+   * @param name the names of the ports to be removed
+   */
   public void removeInputs(List<String> names);
   
+  /**
+   * Retrieve the input port having the specified name. Or null if this port
+   * is not present.
+   * @param name the name of the port we want to retrieve
+   * @return the port having the given name, or null if not present
+   */
   public Port getInput( String name );
   
+  /**
+   * Retrieve the list of all input ports.
+   * @return the list of all input ports
+   */
   public List<Port> getInputs();
 
-  // --- Outputs
+  //================================================ Outputs
+  /**
+   * Add an output port to the module. This port will not be copied, but
+   * rather shared (i.e. you can modify it, but be sure to synchronize
+   * it beforehand)
+   */
   public void addOutput(Port output);
 
+  /**
+   * Add output ports to the module. These ports will not be copied, but
+   * rather shared (i.e. you can modify them, but be sure to synchronize
+   * them beforehand)
+   */
   public void addOutputs(List<Port> outputs);
 
+  /**
+   * The port with the specified name will be removed from the list of
+   * output ports.
+   * @param name the name of the port to be removed
+   */
   public void removeOutput(String name);
 
+  /**
+   * The ports with the specified names will be removed from the list of
+   * output ports.
+   * @param name the names of the ports to be removed
+   */
   public void removeOutputs(List<String> names);
   
   public Port getOutput( String name );
 
   public List<Port> getOutputs();
   
-  // --- Computation
+  //================================================ Computation
   public void compute();
   
-  // --- Cloneable
+  //================================================ Cloneable
   public Module clone() throws CloneNotSupportedException;
 }
