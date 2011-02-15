f89b9f5b5693062e9fa1f6bf84ee6ce02cad7cbc
vallee.aurelien <vallee.aurelien@71548d6f-1a6a-e512-57bc-3977d4f6448c>
2011-02-15 02:14:25 +0000

    Improve sequencer, now playing muse ;)

--- Synthlab/src/synthlab/internal/modules/ModuleSequencer.java
+++ Synthlab/src/synthlab/internal/modules/ModuleSequencer.java
@@ -12,13 +12,10 @@ public class ModuleSequencer extends BasicModule
   {
     super("Sequencer");
 
-    addInput(new BasicPort("iTempo", 15, Port.ValueType.DISCRETE,
+    addInput(new BasicPort("iTempo", 6, Port.ValueType.DISCRETE,
         Port.ValueUnit.MILLISECONDS, new Port.ValueRange(1, 100),
         "Octave modulation input following the 1v/octave convention"));
 
-    addOutput(new BasicPort("oOctave", 3, Port.ValueType.DISCRETE,
-        Port.ValueUnit.VOLT, new Port.ValueRange(0, 7, 7),
-        "Octave modulation input following the 1v/octave convention"));
     addOutput(new BasicPort("oSignal", 0, Port.ValueType.CONTINUOUS,
         Port.ValueUnit.VOLT, new Port.ValueRange(0, 8),
         "Output note voltage following the 1v/octave convention"));
@@ -33,48 +30,48 @@ public class ModuleSequencer extends BasicModule
   public void compute()
   {
 
-    //double[] melodyNescafe = { 17, 17, 17, 13, 15, 15, 15, 18, 17, 17, 17, 13,
-    //    15, 15, 15, 18, 17, 17, 17, 13, 15, 15, 12, 8, 10, 10, 5, 17, 15, 13,
-    //    12 };
-    double[] melodyMorricone = { 9, 14, 9, 14, 9, 9, 9, 9, 9, 9, 9, 9, 5, 5, 5,
-        5, 7, 7, 7, 7, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 9, 14,
-        9, 14, 9, 9, 9, 9, 9, 9, 9, 9, 5, 5, 5, 5, 7, 7, 7, 7, 12, 12, 12, 12,
-        12, 12, 12, 12, 12, 12, 12, 12, 9, 14, 9, 14, 14, 14, 14, 14, 14, 14,
-        14, 14, 5, 5, 5, 5, 4, 4, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9,
-        14, 9, 14, 9, 9, 9, 9, 9, 9, 7, 7, 7, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-        2, 2, 2, 2, 2, 2, 2, 2 };
+    // double[] melodyNescafe = { 17, 17, 17, 13, 15, 15, 15, 18, 17, 17, 17,
+    // 13,
+    // 15, 15, 15, 18, 17, 17, 17, 13, 15, 15, 12, 8, 10, 10, 5, 17, 15, 13,
+    // 12 };
+    // double[] melodyMorricone = { 9, 14, 9, 14, 9, 9, 9, 9, 9, 9, 9, 9, 5, 5,
+    // 5,
+    // 5, 7, 7, 7, 7, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 9, 14,
+    // 9, 14, 9, 9, 9, 9, 9, 9, 9, 9, 5, 5, 5, 5, 7, 7, 7, 7, 12, 12, 12, 12,
+    // 12, 12, 12, 12, 12, 12, 12, 12, 9, 14, 9, 14, 14, 14, 14, 14, 14, 14,
+    // 14, 14, 5, 5, 5, 5, 4, 4, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9,
+    // 14, 9, 14, 9, 9, 9, 9, 9, 9, 7, 7, 7, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+    // 2, 2, 2, 2, 2, 2, 2, 2 };
+    double[] melodyMuse = { 12, 15, 19, 24, 27, 31, 36, 39, 43, 39, 36, 31, 27,
+        24, 19, 15, 10, 14, 17, 22, 26, 29, 34, 38, 41, 38, 34, 29, 26, 22, 17,
+        14, 5, 8, 12, 17, 20, 24, 29, 32, 36, 32, 29, 24, 20, 17, 12, 8 };
     synchronized (getInput("iTempo"))
     {
       synchronized (getOutput("oActive"))
       {
-        synchronized (getOutput("oOctave"))
+        synchronized (getOutput("oSignal"))
         {
-          synchronized (getOutput("oSignal"))
+          double note = 0;
+          for (int i = 0; i < Scheduler.SamplingBufferSize; ++i)
           {
-            double note = 0;
-            for (int i = 0; i < Scheduler.SamplingBufferSize; ++i)
-            {
-              ++sampleCount_;
-              sampleCount_ %= 44100 * getInput("iTempo").getValues().getDouble(
-                  i * (Double.SIZE / 8));
-              getOutput("oOctave").getValues().putDouble(i * (Double.SIZE / 8),
-                  3.0);
-              note = melodyMorricone[(int) Math
-                  .floor((sampleCount_ / (44100. * getInput("iTempo")
-                      .getValues().getDouble(i * (Double.SIZE / 8))))
-                      * (double) melodyMorricone.length)];
-              getOutput("oSignal").getValues().putDouble(i * (Double.SIZE / 8),
-                  note);
+            ++sampleCount_;
+            sampleCount_ %= 44100 * getInput("iTempo").getValues().getDouble(
+                i * (Double.SIZE / 8));
+            note = melodyMuse[(int) Math
+                .floor((sampleCount_ / (44100. * getInput("iTempo").getValues()
+                    .getDouble(i * (Double.SIZE / 8))))
+                    * (double) melodyMuse.length)];
+            getOutput("oSignal").getValues().putDouble(i * (Double.SIZE / 8),
+                note);
 
-            }
-            if (previousNote_ != note)
-            {
-              getOutput("oActive").setValues(0);
-            }
-            else
-              getOutput("oActive").setValues(1);
-            previousNote_ = note;
           }
+          if (previousNote_ != note)
+          {
+            getOutput("oActive").setValues(0);
+          }
+          else
+            getOutput("oActive").setValues(1);
+          previousNote_ = note;
         }
       }
     }
