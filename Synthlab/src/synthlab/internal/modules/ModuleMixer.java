d12f7c2f18664842051278ac08514bfa9d33a330
matian85 <matian85@71548d6f-1a6a-e512-57bc-3977d4f6448c>
2011-02-16 15:14:28 +0000

    update mixer intervalle d'entree gain

--- Synthlab/src/synthlab/internal/modules/ModuleMixer.java
+++ Synthlab/src/synthlab/internal/modules/ModuleMixer.java
@@ -22,17 +22,17 @@ public class ModuleMixer extends BasicModule
   {
     super("MIXER");
     // Input1 & his gain
-    addInput(new BasicPort("iInput1", 0, Port.ValueType.CONTINUOUS, Port.ValueUnit.AMPLITUDE, new Port.ValueRange(-1, 1),"Input sound wave 1"));
-    addInput(new BasicPort("iGain1", 0.5, Port.ValueType.CONTINUOUS, Port.ValueUnit.PERCENTAGE, new Port.ValueRange(0, 1),"Gain (volume) modifier for amplitude of sound wave 1"));
+    addInput(new BasicPort("iInput1", 0, Port.ValueType.CONTINUOUS, Port.ValueUnit.AMPLITUDE, new Port.ValueRange(0, 1),"Input sound wave 1"));
+    addInput(new BasicPort("iGain1", 0.5, Port.ValueType.CONTINUOUS, Port.ValueUnit.PERCENTAGE, new Port.ValueRange(0, 100),"Gain (volume) modifier for amplitude of sound wave 1"));
     // Input2 & his gain
     addInput(new BasicPort("iInput2", 0, Port.ValueType.CONTINUOUS, Port.ValueUnit.AMPLITUDE, new Port.ValueRange(-1, 1),"Input sound wave 1"));
-    addInput(new BasicPort("iGain2", 0.5, Port.ValueType.CONTINUOUS, Port.ValueUnit.PERCENTAGE, new Port.ValueRange(0, 1),"Gain (volume) modifier for amplitude of sound wave 1"));
+    addInput(new BasicPort("iGain2", 0.5, Port.ValueType.CONTINUOUS, Port.ValueUnit.PERCENTAGE, new Port.ValueRange(0, 100),"Gain (volume) modifier for amplitude of sound wave 1"));
     // Input3 & his gain
     addInput(new BasicPort("iInput3", 0, Port.ValueType.CONTINUOUS, Port.ValueUnit.AMPLITUDE, new Port.ValueRange(-1, 1),"Input sound wave 1"));
-    addInput(new BasicPort("iGain3", 0.5, Port.ValueType.CONTINUOUS, Port.ValueUnit.PERCENTAGE, new Port.ValueRange(0, 1),"Gain (volume) modifier for amplitude of sound wave 1"));
+    addInput(new BasicPort("iGain3", 0.5, Port.ValueType.CONTINUOUS, Port.ValueUnit.PERCENTAGE, new Port.ValueRange(0, 100),"Gain (volume) modifier for amplitude of sound wave 1"));
     // Input4 & his gain
     addInput(new BasicPort("iInput4", 0, Port.ValueType.CONTINUOUS, Port.ValueUnit.AMPLITUDE, new Port.ValueRange(-1, 1),"Input sound wave 1"));
-    addInput(new BasicPort("iGain4", 0.5, Port.ValueType.CONTINUOUS, Port.ValueUnit.PERCENTAGE, new Port.ValueRange(0, 1),"Gain (volume) modifier for amplitude of sound wave 1"));
+    addInput(new BasicPort("iGain4", 0.5, Port.ValueType.CONTINUOUS, Port.ValueUnit.PERCENTAGE, new Port.ValueRange(0, 100),"Gain (volume) modifier for amplitude of sound wave 1"));
     
     //Output
     addOutput(new BasicPort("oSignal", 0, Port.ValueType.CONTINUOUS, Port.ValueUnit.AMPLITUDE, new Port.ValueRange(-1, 1),"Output sound wave mixed (averaged) by all inputs/gains"));
