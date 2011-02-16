e82478ccf15068a303303f170deac1a874da06f3
bonjouryoyo <bonjouryoyo@71548d6f-1a6a-e512-57bc-3977d4f6448c>
2011-02-16 15:23:14 +0000

    beginning outToFile

---
+++ Synthlab/src/synthlab/internal/modules/ModuleOutToFile.java
@@ -0,0 +1,51 @@
+package synthlab.internal.modules;
+
+import java.nio.*;
+import javax.sound.sampled.*;
+
+import synthlab.api.*;
+import synthlab.api.Port;
+import synthlab.internal.*;
+
+public class ModuleOutToFile extends BasicModule
+{
+  public ModuleOutToFile()
+  {
+    super("Out");
+
+    addInput(new BasicPort("iSignal", 0, Port.ValueType.CONTINUOUS,
+        Port.ValueUnit.AMPLITUDE, new Port.ValueRange(-1, 1),"Input sound waveto be sent to the sound card"));
+    
+    
+    data_ = ByteBuffer.allocate(Scheduler.SamplingBufferSize * 2);
+  }
+
+  @Override
+  public void compute()
+  {
+    synchronized (getInput("iSignal"))
+    {
+      getInput("iSignal").getValues().clear();
+
+      for (int i = 0; i < Scheduler.SamplingBufferSize; ++i)
+      {
+        data_.putShort(i * 2, (short) (getInput("iSignal").getValues()
+            .getDouble(i * 8) * Short.MAX_VALUE));
+      }
+
+      Audio.getLine().write(data_.array(), 0, Scheduler.SamplingBufferSize * (Short.SIZE/8));
+
+      getInput("iSignal").getValues().clear();
+    }
+  }
+  
+  @Override
+  public Module clone() throws CloneNotSupportedException
+  {
+    return new ModuleOutToFile();
+  }
+
+  AudioInputStream stream_;
+  DataLine.Info    lineInfo_;
+  ByteBuffer       data_;
+}
