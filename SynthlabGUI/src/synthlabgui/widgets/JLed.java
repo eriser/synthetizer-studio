a32ce11a8e2e21b0c677bb3b7f10bb73c49accdd
matian85 <matian85@71548d6f-1a6a-e512-57bc-3977d4f6448c>
2011-02-24 14:10:25 +0000

    ajoute de commentaire

--- SynthlabGUI/src/synthlabgui/widgets/JLed.java
+++ SynthlabGUI/src/synthlabgui/widgets/JLed.java
@@ -5,91 +5,94 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.util.Observable;
 import java.util.Observer;
-
 import javax.swing.JPanel;
-
 import synthlab.api.Port;
 
 /**
- * gerer l'état de la led allume/éteinte
+ * A indicator of two colors that show module's working state.
  * 
  */
-public class JLed extends JPanel implements Observer {
+public class JLed extends JPanel implements Observer
+{
+  private static final long serialVersionUID = 1L;
 
-    private static final long serialVersionUID = 1L;
+  /**
+   * state
+   */
+  private boolean           allume;
 
-    /**
-     * état de led
-     */
-    private boolean allume;
+  private Color             colorOn          = new Color(100, 200, 100);
 
-    private Color colorOn = new Color(100, 200, 100);
-    private Color colorOff = new Color(200, 50, 50);
+  private Color             colorOff         = new Color(200, 50, 50);
 
-    /**
-     * Constructeur par défaut
-     */
-    public JLed() {
-	Dimension size = new Dimension(13, 8);
-	setMinimumSize(size);
-	setMaximumSize(size);
-	setPreferredSize(size);
-	setSize(size);
-	allume = false;
-	setOpaque(false);
+  /**
+   * Constructor
+   */
+  public JLed()
+  {
+    Dimension size = new Dimension(13, 8);
+    setMinimumSize(size);
+    setMaximumSize(size);
+    setPreferredSize(size);
+    setSize(size);
+    allume = false;
+    setOpaque(false);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public void setOn()
+  {
+    allume = true;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public void setOff()
+  {
+    allume = false;
+  }
+
+  /**
+   * @return vrai si la led est allumé, faux sinon.
+   */
+  public boolean isOn()
+  {
+    return allume;
+  }
+
+  /**
+   * @param allume
+   */
+  public void setState(boolean allume)
+  {
+    this.allume = allume;
+  }
+
+  @Override
+  public void paint(Graphics g)
+  {
+    if (allume)
+    {
+      g.setColor(colorOn);
     }
-
-    /**
-     * {@inheritDoc}
-     */
-    public void allumer() {
-	allume = true;
+    else
+    {
+      g.setColor(colorOff);
     }
+    g.fillRoundRect(0, 0, getWidth(), getHeight(), 5, 5);
+  }
 
-    /**
-     * {@inheritDoc}
-     */
-    public void eteindre() {
-	allume = false;
-    }
-
-    /**
-     * @return vrai si la led est allumé, faux sinon.
-     */
-    public boolean isAllume() {
-	return allume;
-    }
-
-    /**
-     * @param allume
-     */
-    public void setAllume(boolean allume) {
-	this.allume = allume;
-    }
-
-    /**
-     * Dessine la LED.
-     * 
-     * TODO A changer
-     */
-    @Override
-    public void paint(Graphics g) {
-	if (allume) {
-	    g.setColor(colorOn);
-	} else {
-	    g.setColor(colorOff);
-	}
-	g.fillRoundRect(0, 0, getWidth(), getHeight(), 5, 5);
-    }
-
-    @Override
-    public void update(Observable o, Object arg) {
-	double value = ((Port) o).getValues().getDouble(0);
-	if (value > 0)
-	    allumer();
-	else
-	    eteindre();
-	repaint();
-    }
-
+  @Override
+  public void update(Observable o, Object arg)
+  {
+    double value = ((Port) o).getValues().getDouble(0);
+    if (value > 0)
+      setOn();
+    else
+      setOff();
+    repaint();
+  }
 }
